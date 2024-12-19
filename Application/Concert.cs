0f5838427657c753ce6bb8bcfae5477b5ff09436
JayB111 <jay.ljungby@gmail.com>
2024-12-19 10:11:48 +0100

    first commit

---
+++ Application/Concert.cs
@@ -0,0 +1,16 @@
+﻿namespace Application
+{
+    public class Concert
+    {
+        public string Name { get; set; }
+        public string Venue { get; set; }
+        public DateTime Date { get; set; }
+        public Concert(string name, string venue, DateTime date)
+        {
+            Name = name;
+            Venue = venue;
+            Date = date;
+
+        }
+    }
+}
--- Application/Program.cs
+++ Application/Program.cs
@@ -1,6 +1,19 @@
+using Application;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<Concert> concerts = new List<Concert>();
+app.MapPost("/add-concert", async (HttpContext context) =>
+{
+    var form = await context.Request.ReadFormAsync();
+    var name = form["name"];
+    var venue = form["venue"];
+    var date = form["date"];
+    var concert = new Concert(name, venue, DateTime.Parse(date));
+    concerts.Add(concert);
+    return Results.Ok("/concert added");
+}); app.MapGet("/", () => "Hello World!");
 
 app.Run();
